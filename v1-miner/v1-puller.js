db82787fb15471e5bb3d85e57a65c04ded11cea6
Scerelyn <scerelyn@gmail.com>
2025-02-08 23:52:32 -0600

    add awaits and fix link to get the js and not the html

--- v1-miner/v1-puller.js
+++ v1-miner/v1-puller.js
@@ -1,7 +1,7 @@
 /** @param {NS} ns */
 export async function main(ns) {
-  ns.wget("https://github.com/Scerelyn/BurnedToBits/blob/main/v1-miner/v1-deployer.js", "v1-deployer.js");
-  ns.wget("https://github.com/Scerelyn/BurnedToBits/blob/main/v1-miner/v1-grow.js", "v1-grow.js");
-  ns.wget("https://github.com/Scerelyn/BurnedToBits/blob/main/v1-miner/v1-hack.js", "v1-hack.js");
-  ns.wget("https://github.com/Scerelyn/BurnedToBits/blob/main/v1-miner/v1-weak.js", "v1-weak.js");
+  await ns.wget("https://raw.githubusercontent.com/Scerelyn/BurnedToBits/refs/heads/main/v1-miner/v1-deployer.js", "v1-miner/v1-deployer.js");
+  await ns.wget("https://raw.githubusercontent.com/Scerelyn/BurnedToBits/refs/heads/main/v1-miner/v1-grow.js", "v1-miner/v1-grow.js");
+  await ns.wget("https://raw.githubusercontent.com/Scerelyn/BurnedToBits/refs/heads/main/v1-miner/v1-hack.js", "v1-miner/v1-hack.js");
+  await ns.wget("https://raw.githubusercontent.com/Scerelyn/BurnedToBits/refs/heads/main/v1-miner/v1-weak.js", "v1-miner/v1-weak.js");
 }
